9d26ddab65ef7c05a82164e61419dec84ca80acc
Leila Moraes <leila.violino@gmail.com>
2023-05-05 11:02:22 -0300

    feat: MotorcycleService getAll and getById

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,9 +1,10 @@
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
 
 export default class MotorcycleService {
-  private createCarDomain(motor: IMotorcycle | null): Motorcycle | null {
+  private createMotorcycleDomain(motor: IMotorcycle | null): Motorcycle | null {
     if (motor) {
       return new Motorcycle(motor);
     }
@@ -13,7 +14,29 @@ export default class MotorcycleService {
   public async create(motor: IMotorcycle) {
     const model = new MotorcycleODM();
     const newMotor = await model.create(motor);
-    const message = this.createCarDomain(newMotor);
+    const message = this.createMotorcycleDomain(newMotor);
     return { status: 201, message };
+  }
+
+  public async getAll() {
+    const model = new MotorcycleODM();
+    const motorcycles = await model.getAll();
+    const message = motorcycles.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
+    return { status: 200, message };
+  }
+
+  public async getById(id: string) {
+    const model = new MotorcycleODM();
+    if (!isValidObjectId(id)) {
+      const message = { message: 'Invalid mongo id' };
+      return { status: 422, message };
+    }
+    const motorcycle = await model.getById(id);
+    if (!motorcycle) {
+      const message = { message: 'Car not found' };
+      return { status: 404, message };
+    }
+    const message = this.createMotorcycleDomain(motorcycle);
+    return { status: 200, message };
   }
 }
