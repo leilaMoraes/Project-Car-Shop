741b2ba8446958365e2833c0decdc23a0f7526a8
Leila Moraes <leila.violino@gmail.com>
2023-05-04 22:56:09 -0300

    refactor: service, para corrigir o retorno

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,10 +1,19 @@
+import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 export default class CarService {
+  private createCarDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car(car);
+    }
+    return null;
+  }
+
   public async create(car: ICar) {
     const model = new CarODM();
     const newCar = await model.create(car);
-    return { status: 201, message: newCar };
+    const message = this.createCarDomain(newCar);
+    return { status: 201, message };
   }
 }
